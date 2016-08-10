b73925dfae0e02199b1f5ba5ebb659bb796169cf
shrimpfriedrice <cookiefriedrice@gmail.com>
2016-08-10 15:24:22 +0530

    Restructure to include other lab tools

---
+++ simple-ocr/indexCapture_voltage.py
@@ -0,0 +1,59 @@
+#DELETE OLD INDEX FILE BEFORE USING THIS
+import cv2
+import numpy as np
+import pickle
+
+file_select = raw_input("give me food\n")
+#file_select = 'h3s2.mov'
+cap = cv2.VideoCapture(file_select)
+#cap = cv2.VideoCapture('heatedrun3session2.MOV')
+
+ix, iy = -1, -1
+locks = []
+#locksx, locksy = [], []
+# mouse callback function
+def mouse_click(event, x, y, flags, param):
+    global ix, iy
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img,(x,y),100,(255,0,0),-1)
+        ix,iy = x,y
+
+cv2.namedWindow('frame')
+cv2.setMouseCallback('frame', mouse_click)
+while cap.isOpened():
+
+    ret, frame = cap.read()
+
+    img = np.zeros((512,512,3), np.uint8)
+    cv2.circle(img, (3,4), 200, (0,0,255), -1)
+
+    cv2.imshow('frame', frame)
+    k = cv2.waitKey(10000) & 0xFF
+    if k == 27:
+        break
+    elif k == ord('s'):
+        break
+    elif k == ord('a'):
+        locks.append((ix, iy))
+        # locksx.append(ix)
+        # locksy.append(iy)
+        print ix, iy
+        print frame[iy, ix]
+    elif k == ord('b'):
+        # pkl_input_filex = open(file_select + 'x', 'wb')
+        # pkl_input_filey = open(file_select + 'y', 'wb')
+        # pickle.dump(locksx, pkl_input_filex)
+        # pickle.dump(locksy, pkl_input_filey)
+        # pkl_input_filex.close()
+        # pkl_input_filey.close()
+        if len([locks[i:i+7] for i in range(0, len(locks), 7)]) != 3:
+            print 'something is wrong ...'
+            print len([locks[i:i+7] for i in range(0, len(locks), 7)])
+        else:
+            pkl_input_file = open(file_select + 'index_voltage', 'wb')
+            pickle.dump(locks, pkl_input_file)
+            pkl_input_file.close()
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ simple-ocr/videoAnalytics.py
@@ -0,0 +1,120 @@
+import cv2
+import numpy as np
+import pickle
+import csv
+
+#file_select = raw_input("give me food\n")
+file_select = 'h3s2.mov'
+cap = cv2.VideoCapture(file_select)
+previousMassVal = 210.0 #211 for h2s2 210 for rest
+aD = 0.5
+tC = 0
+#cap = cv2.VideoCapture('heatedrun3session2.MOV')
+
+ix, iy = -1, -1
+threshold = 150 #150 for h3s2, 180 for rest, 140 for u2s3
+checkThreshold = False
+
+cv2.namedWindow('frame')
+# pkl_output_filex = open(file_select+'x', 'rb')
+# pkl_output_filey = open(file_select+'y', 'rb')
+# locksx = pickle.load(pkl_output_filex)
+# locksy = pickle.load(pkl_output_filey)
+pkl_output_file = open(file_select+'index', 'rb')
+locks = pickle.load(pkl_output_file)
+digits = [locks[i:i+7] for i in range(0, len(locks), 7)]
+print digits
+print len(digits)
+one = [0,1,0,0,1,0,0]
+two = [1,1,0,1,0,1,1]
+three = [1,1,0,1,1,0,1]
+four = [0,1,1,1,1,0,0]
+five = [1,0,1,1,1,0,1]
+six = [1,0,1,1,1,1,1]
+seven = [1,1,0,0,1,0,0]
+eight = [1,1,1,1,1,1,1]
+nine = [1,1,1,1,1,0,1]
+zero = [1,1,1,0,1,1,1]
+mass = []
+masscheck = []
+masscheck.append(previousMassVal)
+#oldreading = 0
+while cap.isOpened():
+
+    ret, frame = cap.read()
+
+
+    count = 0
+    toPrint = True
+    reading = []
+    for digit in digits:
+        identity = []
+        for lock in digit:
+            pixel = frame[lock[1], lock[0]]
+            checksum = ((int(pixel[0])) + (int(pixel[1])))/2.0
+            if checksum <= threshold:
+                if checkThreshold:
+                    print 'inside'
+                identity.append(1)
+            else:
+                identity.append(0)
+        if np.array_equal(identity, one):
+            reading.append(1)
+        elif np.array_equal(identity, two):
+            reading.append(2)
+        elif np.array_equal(identity, three):
+            reading.append(3)
+        elif np.array_equal(identity, four):
+            reading.append(4)
+        elif np.array_equal(identity, five):
+            reading.append(5)
+        elif np.array_equal(identity, six):
+            reading.append(6)
+        elif np.array_equal(identity, seven):
+            reading.append(7)
+        elif np.array_equal(identity, eight):
+            reading.append(8)
+        elif np.array_equal(identity, nine):
+            reading.append(9)
+        elif np.array_equal(identity, zero):
+            reading.append(0)
+        else:
+            #reading.append(oldreading-1)
+            #print identity
+            #print "AAAAAAAAAAAAAAAA"
+            toPrint = False
+            #break
+
+    if toPrint:
+        massVal = reading[0] * 100.0 + reading[1] * 10.0 + reading[2] + (reading[3]/10.0)
+        if previousMassVal - massVal < aD or tC > 3:
+            tC = 0
+            if massVal <= min(masscheck):
+                mass.append((massVal, (cap.get(0)/1000.0)))
+                masscheck.append(massVal)
+                print str(massVal) + "@" + str(cap.get(0)/1000.0)
+                print mass[-1]
+                fd = open(file_select + '.csv','ab')
+                fd.write(str(cap.get(0)/1000.0) + ',' + str(massVal) + '\n')
+                fd.close()
+            previousMassVal = massVal
+        else:
+            tC += 1
+
+
+    # #oldreading = reading[0]
+    # if toPrint:
+    #     print reading[3]
+    cv2.namedWindow("video stream", cv2.WINDOW_NORMAL)
+    cv2.imshow('frame', frame)
+    cv2.namedWindow("video stream", cv2.WINDOW_NORMAL)
+    #k = cv2.waitKey(1) & 0xFF
+    k = cv2.waitKey(20) & 0xFF
+    #cv2.waitKey(20) &
+    if k == 27:
+        break
+    elif k == ord('s'):
+        break
+print mass
+cap.release()
+cv2.destroyAllWindows()
